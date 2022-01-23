a15cf98569bca01cb16666cca0e468719183c5c4
Vitalina5 <viktoriana15@gmail.com>
2022-01-23 12:18:38 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import './App.css';
+import { data } from './data';
+
+function App() {
+  const [gifts, setGifts] = useState(data);
+  const removeGift =(id) => {
+    const newGifts = gifts.filter(gift => gift.id !== id);
+    setGifts(newGifts);
+  }
+
+  return (
+    <div>
+      <div className='container'>
+        <h1>List of {gifts.length} gifts</h1>
+      </div>
+
+      {gifts.map(element => {
+        const{id, gift, image} = element;
+
+        return  <div key={id}>
+        <div className='container'>
+          <h2>{id} - {gift}</h2>
+        </div>
+        <div className='container'>
+          <img src={image} width="300px" alt="gift" />
+        </div>
+        <div className='container'>
+          <button onClick={() => removeGift(id)}>remove</button>
+        </div>
+        </div>
+      })}
+      < div className='container'>
+         <button onClick={() => setGifts([])} >delete all</button>
+      </div>
+    </div>
+  )
+  
+}
+
+export default App;
